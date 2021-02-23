10d95be1a759c6ddbfd9f6fe48797611528d5901
mohsen <mohsen.zandie1@gmail.com>
2021-02-23 16:33:17 +0330

    Adds home fragment ui; Two buttons

--- app/src/main/java/com/example/codingchallenge35/HomeFragment.kt
+++ app/src/main/java/com/example/codingchallenge35/HomeFragment.kt
@@ -1,17 +1,16 @@
 package com.example.codingchallenge35
 
 import android.os.Bundle
-import android.provider.ContactsContract
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.codingchallenge35.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-    private lateinit var binding : FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
