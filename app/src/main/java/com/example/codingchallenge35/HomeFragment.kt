e9104c5ea5c371471a18dabbc8f1280e4a0ed9f4
mohsen <mohsen.zandie1@gmail.com>
2021-02-23 16:29:02 +0330

    Adds data binding to home fragment

--- app/src/main/java/com/example/codingchallenge35/HomeFragment.kt
+++ app/src/main/java/com/example/codingchallenge35/HomeFragment.kt
@@ -1,14 +1,17 @@
 package com.example.codingchallenge35
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.codingchallenge35.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding : FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,7 +20,8 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
     }
 
 }
