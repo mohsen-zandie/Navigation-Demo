c3ffb628145783d1efdc5e6ae064b4d625f459f9
mohsen <mohsen.zandie1@gmail.com>
2021-02-24 10:24:04 +0330

    adds two way binding for welcome edit text

--- app/src/main/java/com/example/codingchallenge35/ui/WelcomeFragment.kt
+++ app/src/main/java/com/example/codingchallenge35/ui/WelcomeFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.codingchallenge35.R
 import com.example.codingchallenge35.databinding.FragmentWelcomeBinding
+import com.example.codingchallenge35.viewmodel.SharedViewModel
 
 class WelcomeFragment : Fragment() {
     private lateinit var binding: FragmentWelcomeBinding
+    private lateinit var model: SharedViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,6 +27,9 @@ class WelcomeFragment : Fragment() {
         binding.viewTerms.setOnClickListener {
             it.findNavController().navigate(R.id.action_welcomeFragment_to_termsFragment)
         }
+        model = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        binding.viewModel = model
+        binding.lifecycleOwner = requireActivity()
         return binding.root
     }
 }
