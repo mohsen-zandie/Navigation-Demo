74d3ed215e7a8daa10f89858653a1226e948c47f
mohsen <mohsen.zandie1@gmail.com>
2021-02-24 10:15:40 +0330

    Adds two way binding for name edit text

--- app/src/main/java/com/example/codingchallenge35/ui/NameFragment.kt
+++ app/src/main/java/com/example/codingchallenge35/ui/NameFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.codingchallenge35.R
 import com.example.codingchallenge35.databinding.FragmentNameBinding
+import com.example.codingchallenge35.viewmodel.SharedViewModel
 
 class NameFragment : Fragment() {
     private lateinit var binding: FragmentNameBinding
+    private lateinit var model: SharedViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,6 +28,8 @@ class NameFragment : Fragment() {
         binding.next.setOnClickListener {
             it.findNavController().navigate(R.id.action_nameFragment_to_emailFragment)
         }
+        model = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        binding.lifecycleOwner = requireActivity()
         return binding.root
     }
 
