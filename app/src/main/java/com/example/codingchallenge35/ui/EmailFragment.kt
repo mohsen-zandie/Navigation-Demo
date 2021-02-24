9eb857c51b38fd4574ae594cd9bc40b8b83015ae
mohsen <mohsen.zandie1@gmail.com>
2021-02-24 10:19:48 +0330

    adds two way binding for email edit text

--- app/src/main/java/com/example/codingchallenge35/ui/EmailFragment.kt
+++ app/src/main/java/com/example/codingchallenge35/ui/EmailFragment.kt
@@ -6,13 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.codingchallenge35.R
 import com.example.codingchallenge35.databinding.FragmentEmailBinding
+import com.example.codingchallenge35.viewmodel.SharedViewModel
 
 
 class EmailFragment : Fragment() {
     private lateinit var binding: FragmentEmailBinding
+    private lateinit var model : SharedViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -25,6 +28,9 @@ class EmailFragment : Fragment() {
         binding.submit.setOnClickListener {
             it.findNavController().navigate(R.id.action_emailFragment_to_welcomeFragment)
         }
+        model = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+        binding.viewModel = model
+        binding.lifecycleOwner = requireActivity()
         return binding.root
     }
 
